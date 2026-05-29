f5886c6aa9bc7624f041609adb4c3a38645a569c
steezydesta-trim <alexandergdesta@gmail.com>
2026-05-29 17:37:06 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,8 @@
-const CACHE = 'chicago-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'chicago-v2';
+const ASSETS = [
+  '/Chicago-2027/',
+  '/Chicago-2027/index.html'
+];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
